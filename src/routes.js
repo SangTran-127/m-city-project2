a4939c4d078d60c0d9380e8baca9caa48029f69e
Trần Quang Sáng <tranquangsang.127@gmail.com>
2021-06-14 03:27:50 +0700

    upload

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,13 @@ import Header from './Components/Header_Footer/header'
 import Footer from './Components/Header_Footer/footer'
 import Home from './Components/Home'
 import SignIn from './Components/Signin/index'
-
+import Dashboard from './Components/Admin/Dashboard'
 function Routes({user}) {
   return (
     <BrowserRouter>
         <Header user={user}/>
         <Switch>
+            <Route path="/sign_in" exact component={Dashboard}/>
             <Route path="/sign_in" exact component={SignIn}/>
             <Route path="/" exact component={Home}/>
         </Switch>
